c5191f04b851c53a2537213357ed47d688dc2959
jessicaraee <76750070+jessicaraee@users.noreply.github.com>
2025-10-10 13:54:00 -0400

    Update openalex_output_brief.py

--- openalex_output_brief.py
+++ openalex_output_brief.py
@@ -23,16 +23,16 @@ params = {
     'per-page': PER_PAGE,
 }
 
-OUTPUT_FILE = f'/filepath/openalexoutput_{institution_id}_CY{calendar_year}.xlsx'
-OUTPUT_COLUMNS = ['id', 'doi', 'title', 'display_name', 'corresponding_institution_ids']
-
-#Initialize cursor and loop through pages, allowing for wait times if errors
 cursor = "*"
 all_results = []
 count_api_queries = 0
 max_retries = 5
 polite_delay = 1.2
 
+OUTPUT_FILE = f'/filepath/openalexoutput_{institution_id}_CY{calendar_year}.xlsx'
+OUTPUT_COLUMNS = ['id', 'doi', 'title', 'display_name', 'corresponding_institution_ids']
+
+#Fetch data, allowing for retries if errors
 while cursor:
     params["cursor"] = cursor
     retries = 0
